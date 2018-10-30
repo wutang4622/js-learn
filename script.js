184c4b3126b4bad10731c00b73fe8d17384b57c0
wutang4622 <wutangsuper@gmail.com>
2018-10-30 23:16:35 +0200

    this is our first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// console.log(leftBorderWidth);
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// var number = 5;
+// var string = "Hello";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+// let person = {
+//     name : "John",
+//     age : 25,
+//     isMarried : false
+// }
+
+// console.log(person["age"]);
+
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+// console.log(arr[2]);
+
+// let answer = +prompt("Есть ли вам 18","Да");
+// console.log(typeof(answer));
+
+let inc = 10,
+    decr = 10;
+
+// let isChecked = false,
+// isclosed = false;
+
+// console.log(isChecked && isclosed);
